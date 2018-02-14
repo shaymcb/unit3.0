3a512adce409222544213c2e5a135dd3e1df0691
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-02-14 08:12:21 -0500

    Updated from Brython Server: 2/14/2018 8:12:20 AM

--- loopDemo.py
+++ loopDemo.py
@@ -3,5 +3,5 @@
 #loopDemo.py - how to use for and while loops
 
 #I love cs 5 times
-for i in range(0,5): #variable does not need to be i
+for i in range(1,6): #variable does not need to be i, doesn't do last number
     print('I <3 computer science!')
