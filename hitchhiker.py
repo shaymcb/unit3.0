73cd89092b5c06216c1f1e576102d6ac95417b23
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-02-15 08:20:37 -0500

    Updated from Brython Server: 2/15/2018 8:20:35 AM

--- hitchhiker.py
+++ hitchhiker.py
@@ -1,1 +1,9 @@
+#Shaylee McBride
+#15Feb2018
+#hitchhiker.py - the answer is 42
 
+while 1==1:
+    text = input('Ask a question or type "quit": ')
+    if text == 'quit':
+        break
+    print('42')
