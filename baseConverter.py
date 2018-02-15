8c4dbec570a7fd404eb2473496e09e75a32512bd
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-02-15 13:31:27 -0500

    Updated from Brython Server: 2/15/2018 1:31:26 PM

--- baseConverter.py
+++ baseConverter.py
@@ -3,26 +3,31 @@
 #baseConverter.py
 
 base10 = int(input('Enter a base-10 number:' ))
+original = base10
 base = int(input('What base would you like to convert to? '))
-
 
 #find highest place
 division = 1
 places = 0
 while division < base10:
     division = division * base
+    places += 1
 division = division / base
 
+#loop to find place values
 num = ""
 while base10 > 1:
     place = base10//division
-    base10 = base - place*division
-    if place <= 9:
-        place = str(place)
+    base10 = int(base10 - place*division)
+    if place > 9:
+        s = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" #accomodate for double-digits
+        place = s[place-10]
     else:
-        s = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        place = s[place-9]
+        place = str(place)
     num = num+place
     division = division / base
+    places -= 1
 
-print(num)
+#account for zeros
+num = num+"0"*places
+print(original,'in base',base,'is',num)
