b2fbfb4ee2e494afb9373829b0e5273f0db1604c
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-02-15 08:17:26 -0500

    Updated from Brython Server: 2/15/2018 8:17:25 AM

--- guass.py
+++ guass.py
@@ -1,1 +1,12 @@
+#Shaylee McBride
+#15Feb2018
+#gauss.py - adds up numbers
 
+num1 = int(input('Enter lower bound: '))
+num2 = int(input('Enter upper bound: '))
+step = int(input('Enter step: '))
+
+total = 0
+for i in range(num1,num2+1,step):
+    total += i
+print(total)
