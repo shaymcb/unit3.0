042016ee98b6300fcbbbbcdd7a7d54e9c3b0b3d6
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-02-15 08:48:31 -0500

    Updated from Brython Server: 2/15/2018 8:48:30 AM

--- fri13.py
+++ fri13.py
@@ -1,1 +1,16 @@
+#Shaylee McBride
+#15Feb2018
+#fri13.py
 
+from calendar import weekday
+from datetime import date
+
+year = date.today().year
+month = date.today().month
+dates = 0
+i = 0
+
+while dates <=10
+    i += 1
+    weekday(year,month,13)
+    
