dcf441f88e1011d7d5ecb2bfa446c33890355724
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-02-15 08:53:57 -0500

    Updated from Brython Server: 2/15/2018 8:53:56 AM

--- fri13.py
+++ fri13.py
@@ -8,10 +8,13 @@ from datetime import date
 year = date.today().year
 month = date.today().month
 dates = 0
-i = 0
 
-while dates <=10
-    i += 1
+while dates <= 10:
     weekday = weekday(year,month,13)
-    if weekday = 5:
+    if weekday == 5:
         print(month+'/13/'+year)
+        dates += 1
+    month += 1
+    if month == 13:
+        year += 1
+        month = 1
