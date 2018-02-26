61114a4e718258a3e4fabecf41acfec537967e59
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-02-26 11:50:20 -0500

    Updated from Brython Server: 2/26/2018 11:50:20 AM

--- usa.py
+++ usa.py
@@ -7,6 +7,7 @@ from ggame import *
 red = Color(0xFF0000,1)
 white = Color(0xFFFFFF,1)
 blue = Color(0x0000FF,1)
+black = Color(0x000000,1)
 y=int(input('Enter flag height: ')) #y is flag height, used as scaling factor in like everything so I made it one letter
 x = y/80 #scale factor for stars, probably shouldn't have called it x but whatever
 distx = (0.063*y-4.9*x/2)/y #x distance between stars, initial position is measured from center but python measures from top left so had to adjust that
@@ -21,12 +22,15 @@ outline = LineStyle(0,white)
 star = PolygonAsset([(x*.073,x*2.417),(x*1.927,x*2.417),(x*2.5,x*0.383),(x*3.073,x*2.417),(x*4.927,x*2.417),(x*3.427,x*3.237),(x*4,x*5),(x*2.5,x*3.91),(x*1,x*5),(x*1.573,x*3.237)],outline,white)
 blueRectangle = RectangleAsset(blueWidth,blueHeight,outline,blue)
 redRectangle = RectangleAsset(stripeWidth,y,outline,red)
-whiteStripe = RectangleAsset(stripeWidth,stripeHeight,white)
+whiteStripe = RectangleAsset(stripeWidth,stripeHeight,outline,white)
+background = RectangleAsset(1000,1000,outline,black)
+
+Sprite(background)
 
 #stripes
 Sprite(redRectangle)
-for i in range(1,7):
-    
+for i in range(0,6):
+    Sprite(whiteStripe,(0,(1/13+2*i/13)*y))
 
 #stars
 Sprite(blueRectangle)
