700ae8de642ce2891cc17991413a8a20894a9a50
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-02-26 08:05:30 -0500

    Updated from Brython Server: 2/26/2018 8:05:30 AM

--- stringLoopDemo.py
+++ stringLoopDemo.py
@@ -1,1 +1,7 @@
+#Shaylee McBride
+#26Feb2018
+#stringLoopDemo.py - how to use loops with strings
 
+name = input('Enter name: ')
+for ch in name:  #ch will go through name letter by letter
+    print(ch*7)
