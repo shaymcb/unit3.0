1cc7549fbca9dad0ca1ca092fa1025eb282c831f
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-03-01 08:24:52 -0500

    Updated from Brython Server: 3/1/2018 8:24:51 AM

--- warmup9.py
+++ warmup9.py
@@ -1,1 +1,12 @@
+#Shaylee McBride
+#1March2018
+#warmup9.py
 
+text = input('Enter text: ')
+
+for ch in text:
+    if ch in 'abcde':
+        print(ch.upper())
+    
+    else:
+        print(ch.lower())
