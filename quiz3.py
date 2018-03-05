60c8feb4f64685ab8b7a79c868fc6567e77d6f17
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-03-05 08:19:41 -0500

    Updated from Brython Server: 3/5/2018 8:19:41 AM

--- quiz3.py
+++ quiz3.py
@@ -1,1 +1,19 @@
+#Shaylee McBride
+#5Mar2018
+#quiz3.py
 
+n = -15
+while n <= -9:
+    print(n)
+    n += 1
+
+for i in range(9):
+    print(50 - 4*i)
+
+total = 0
+for i in range(-100,1000+1,2):
+    total += i
+print(total)
+
+while True:
+    text = 
